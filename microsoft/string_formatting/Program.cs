d7ab53ee5bb6e491208d95bcdefbe3528cc0b5b0
michalwalewski <michalwalewski@gmail.com>
2023-08-20 21:34:50 +0000

    Created c# repo

---
+++ microsoft/string_formatting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello\nWorld!");
+	    Console.WriteLine("Hello\nWorld!");
+	    Console.WriteLine("Hello\tWorld!");
+	    Console.WriteLine("Hello\"World\"!");
+	    Console.WriteLine("c:\\Program Files\\Chrome");
+	    Console.WriteLine("c:\\windows\\system32\ndrivers");
+	    Console.WriteLine("test\ttest1\ntest2\rtest3");	    
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ microsoft/vars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    
+            //Console Output
+            Console.WriteLine("Hello World!");
+	    Console.WriteLine(true);
+	    Console.WriteLine(1234);
+	    Console.Write("No new line");
+	    Console.WriteLine("New Line");
+
+            //Variables
+	    string sFirstName;
+	    sFirstName="Bob";
+            Console.WriteLine(sFirstName);
+
+	    sFirstName = "Bob";
+	    Console.WriteLine(sFirstName);
+            sFirstName = "Liem";
+            Console.WriteLine(sFirstName);
+            sFirstName = "Isabella";
+            Console.WriteLine(sFirstName);
+            sFirstName = "Yasmin";
+            Console.WriteLine(sFirstName);
+
+        string sTextVariable = "Bob2";
+	    Console.WriteLine(sTextVariable);
+
+        }
+    }
+}
